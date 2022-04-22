54e154c1b344f3470dd60929437dc314f13a3c21
rolando22 <rolando2207@gmail.com>
2022-04-21 23:30:52 -0300

    Setup de interfaz y conexión a redes

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
-import Web3 from "web3/dist/web3.min";
+import MainLayout from "./layouts/main";
 
 function App() {
-  useEffect(() => {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      web3.eth.requestAccounts().then(console.log);
-    }
-  }, []);
   return (
     <>
-      <Routes>
-        <Route path="/" element={<Home />} />
-      </Routes>
+      <MainLayout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </MainLayout>
     </>
   );
 }
