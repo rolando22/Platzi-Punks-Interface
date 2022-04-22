6846a08b35a3fb06a4e2afcd9dbe417edbbbeb42
rolando22 <rolando2207@gmail.com>
2022-04-21 21:10:03 -0300

    Setup para conexión con Metamask

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
+import { useEffect } from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
+import Web3 from "web3/dist/web3.min";
 
 function App() {
+  useEffect(() => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      web3.eth.requestAccounts().then(console.log);
+    }
+  }, []);
   return (
     <>
       <Routes>
