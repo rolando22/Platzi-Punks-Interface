0e16fbdd1aafc0e92d0155bad5069dce0d1fdd31
rolando22 <rolando2207@gmail.com>
2022-04-23 21:43:26 -0300

    Visualización del detalle de los PlatziPunks

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { Route, Routes } from "react-router-dom";
 import Home from "./views/home";
 import Punks from "./views/punks";
+import Punk from "./views/punk";
 import MainLayout from "./layouts/main";
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/punks" element={<Punks />}/>
+          <Route path="/punks/:tokenId" element={<Punk />}/>
         </Routes>
       </MainLayout>
     </>
